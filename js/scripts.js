a0f41fac7116b386ddd97f2af1fb5d768e230a27
Wingo <cmwingo@gmail.com>
2016-10-18 23:27:38 -0700

    Add slide function

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,13 @@ $(document).ready(function(){
     $(".col-md-12").children("h1").last().after("<img src=img/break.jpg alt=A cat and dog laying together>");
     $("h1#breakTime").toggle();
     $("#fight").toggle();
+    $(".col-md-6").slideUp();
   });
 
   $(".clickable#fight").click(function(){
     $(".col-md-12 img").remove();
     $("h1#breakTime").toggle();
     $("#fight").toggle();
+    $(".col-md-6").slideDown();
   });
 });
