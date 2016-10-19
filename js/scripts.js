2e304cd9dd77f0269444c0da609bd93944a71cc5
Wingo <cmwingo@gmail.com>
2016-10-18 22:49:46 -0700

    Add disappearing break time image

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,15 @@ $(document).ready(function(){
     $("#dogSide h2").after("<h3>Rrrrr...WOOF WOOF!</h3>");
   });
 
+  $(".clickable#break").click(function(){
+    $(".col-md-12").children("h1").last().after("<img src=img/break.jpg alt=A cat and dog laying together>");
+    $("h1#breakTime").toggle();
+    $("#fight").toggle();
+  });
+
+  $(".clickable#fight").click(function(){
+    $(".col-md-12 img").remove();
+    $("h1#breakTime").toggle();
+    $("#fight").toggle();
+  });
 });
